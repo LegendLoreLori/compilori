96c12b8a71c06a4161d9f4d968707eb203e01491
Lori <lorenzomandolfino@gmail.com>
2024-11-04 08:41:50 +0800

    add lexer
    - add `lexer()` function
    - update driver.py to accept additional optional arguments, `lex`,
      `parse`, and `codegen`
    - update main function to interpret optional arguments
    - install and apply black formatter, reformat driver.py to black
      standard

--- src/driver.py
+++ src/driver.py
@@ -2,41 +2,96 @@
 
 import subprocess
 import argparse
+import re
 
 parser = argparse.ArgumentParser(
     description="Handles the execution of all the separate parts of compilation",
-    epilog="help"
+    epilog="help",
 )
 
 parser.add_argument("filename", help="The input file to preprocess")
-parser.add_argument("--lex", action="store_const", const=1, help="Run lexer, but stop before parsing")
-parser.add_argument("--parse", action="store_const", const=1, help="Run lexer and parser, but stop before assembling")
-parser.add_argument("--codegen", action="store_const", const=1,help="Run lexing, parsing, and assembly generation, but stop before code emission")
+parser.add_argument(
+    "--lex", action="store_const", const=1, help="Run lexer, but stop before parsing"
+)
+parser.add_argument(
+    "--parse",
+    action="store_const",
+    const=1,
+    help="Run lexer and parser, but stop before assembling",
+)
+parser.add_argument(
+    "--codegen",
+    action="store_const",
+    const=1,
+    help="Run lexing, parsing, and assembly generation, but stop before code emission",
+)
 
 args = parser.parse_args()
 
-def preprocess(filename):
+TOKEN_PATTERNS = {
+    # "identifier": re.compile(r"[a-zA-Z_]\w*\b"),
+    "constant": re.compile(r"[0-9]+\b"),
+    "keyword": re.compile(r"[int\bvoid\breturn\b]"),
+    "symbol": re.compile(r"[\(\){};]"),
+}
+
+
+def preprocess(filename) -> str:
     """Prepare a file to be compiled by performing a preprocessing pass over the source file,
     writing the output to a temporary file"""
     preprocessed_filename = filename.replace(".c", ".i")
-    with open(preprocessed_filename, 'w+', encoding='utf-8') as f:
+    with open(preprocessed_filename, "w+", encoding="utf-8") as f:
         subprocess.run(["gcc", "-E", "-P", filename], check=True, text=True, stdout=f)
+    return preprocessed_filename
+
 
 def compile_c():
     NotImplemented
+
+
+def lexer(filename):
+    """Tokenise contents of a preprocessed file"""
+    with open(filename, encoding="utf-8") as f:
+        source: str = f.read().rstrip()
+
+    tokens = []
+    while source:
+        token = ""
+        if source[0].isspace():
+            source = source.lstrip()
+        # TODO: replace for loop and token patterns with a single regex and reduce iterations
+        for key, pattern in TOKEN_PATTERNS.items():
+            match = pattern.match(source)
+            if match and key == "identifier":
+                print(key)
+                token = (key, match.group())
+                break
+            if match:
+                token = (key, match.group())
+                break
+        if not token:
+            print(source)
+            raise KeyError(
+                [
+                    "Unsupported character provided, unable to tokenise source.",
+                    repr(source),
+                ]
+            )
+        tokens.append(token)
+        source = source.removeprefix(token[-1])
+    return tokens
+
 
 def link():
     NotImplemented
 
 
-
 if __name__ == "__main__":
     if args.lex:
-        # none of these options should produce any output files
-        print("test lex")
+        print(lexer(preprocess(args.filename)))
     if args.parse:
         print("test parse")
     if args.codegen:
         print("test codegen")
-
-    preprocess(args.filename)
+    else:
+        preprocess(args.filename)
